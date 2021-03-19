00d4951e0df402838806fb1fd0d5a926393daf82
Oleg91 <wanone@mail.ru>
2021-03-19 19:36:44 +0300

    hw 1

---
+++ src/HomeWork1/HomeWork1.java
@@ -0,0 +1,38 @@
+package HomeWork1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+//ЗАДАЧА 1
+long start = 1;
+long end = 25;
+boolean res = start < end;
+System.out.println(start<end);
+long between = start < end ? end - start : -1;
+System.out.println(between);
+// ЗАДАЧА 2
+int number =87,a,b,c;
+a = number/10;
+b = number%10;
+System.out.println(a+b);
+//ЗАДАЧА 3
+int num =153;
+a = num/100;
+b = num/10%10;
+c = num%10;
+System.out.println(a+b+c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
